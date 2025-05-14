2f694c7fa728b62627b710bf082f1e96efcdf4dd
Dieguidev <diegogaraycullas@gmail.com>
2025-05-13 21:26:31 -0500

    Corrige la importación de OpenAI para utilizar la ruta correcta

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import OpenAI from "openai/index.mjs";
+import OpenAI from "openai";
 
 dotenv.config();
 
@@ -18,39 +18,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post("/api/traducir", async (req, res) => {
-  const { text, targetLang } = req.body;
-
-  const prompSystem1 = "Eres un traductor profesional.";
-  const prompSystem2 =
-    "Solo puedes responder con una traduccion directa del texto que el usuario te envie." +
-    "Cualquier otra respuesta o conversacion esta prohibida";
-  const prompUser = `Traduce el siguiente texto al ${targetLang}: ${text.trim()}`;
-
-  try {
-    const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: prompSystem1 },
-        { role: "system", content: prompSystem2 },
-        { role: "user", content: prompUser },
-      ],
-      max_tokens: 500,
-      response_format: { type: "text" },
-    });
-
-    const translatedText = completion.choices[0].message.content;
-
-    return res.status(200).json({
-      translatedText,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      error: "Error al traducir el texto",
-    });
-  }
-});
+app.post("/api/nutri-chat", async (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
