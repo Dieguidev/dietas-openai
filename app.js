18c1e3d92ed70275f74cb50157e6ea6b7b81324a
Dieguidev <diegogaraycullas@gmail.com>
2025-05-13 21:59:30 -0500

    Implementa la lógica para manejar el flujo de conversación en la API de nutrición

--- app.js
+++ app.js
@@ -18,7 +18,54 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post("/api/nutri-chat", async (req, res) => {});
+let userData = {};
+
+app.post("/api/nutri-chat", async (req, res) => {
+  const userMessage = req.body.message;
+  const userId = req.body.userId;
+
+  if (!userData[userId]) {
+    userData[userId] = {};
+  }
+
+  if (!userData[userId].peso) {
+    userData[userId].peso = userMessage;
+    return res.json({ reply: "¿Cuánto mides en (cm)?" });
+  }
+
+  if (!userData[userId].altura) {
+    userData[userId].altura = userMessage;
+    return res.json({
+      reply: "¿Cuál es tu objetivo? (adelgazar, mantener o subir de peso)",
+    });
+  }
+
+  if (!userData[userId].objetivo) {
+    userData[userId].objetivo = userMessage;
+    return res.json({ reply: "¿Tienes alguna alergia?" });
+  }
+
+  if (!userData[userId].alergias) {
+    userData[userId].alergias = userMessage;
+    return res.json({ reply: "¿Qué alimentos no te gustan?" });
+  }
+
+  if (!userData[userId].noGusta) {
+    userData[userId].noGusta = userMessage;
+    return res.json({ reply: "¿Cuántas comidas quieres hacer cada dia?" });
+  }
+
+  if (!userData[userId].comidas) {
+    userData[userId].comidas = userMessage;
+  }
+
+  userData[userId] = {};
+
+  return res.json({
+    reply:
+      "Gracias por tus respuestas. Ya tienes tu dieta creada, usa los ingredientes para hacer una receta",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
